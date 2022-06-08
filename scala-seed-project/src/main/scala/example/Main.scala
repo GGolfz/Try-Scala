d86915db8c13cceee009689246bac09d53444cf8
ggolfz <wisarutgolf11@gmail.com>
2022-06-08 09:51:14 +0700

    add: Simple Actor :sparkles:

--- scala-seed-project/src/main/scala/example/Main.scala
+++ scala-seed-project/src/main/scala/example/Main.scala
@@ -1,13 +1,40 @@
 package example
-import akka.actor.{Actor, ActorSystem, Props}
+import akka.actor.SupervisorStrategy.{Restart, Stop, stop}
+import akka.actor.{Actor, ActorLogging, ActorSystem, OneForOneStrategy, PoisonPill, Props, Stash, SupervisorStrategy}
 
 object Main extends App {
-  class SimpleActor extends Actor {
+  class SimpleActor extends Actor with ActorLogging with Stash {
     override def receive: Receive = {
+      case "stashThis" => stash()
+      case "changeHandlerNow" =>
+        unstashAll()
+        context.become(anotherHandler)
+      case "changeHandler" =>
+        context.become(anotherHandler)
       case message: String => println(s"I receive: $message")
+    }
+    def anotherHandler: Receive = {
+      case message => println(s"I receive: $message in another handler")
+    }
+
+    override def preStart(): Unit = {
+      log.info("I'm start")
+    }
+
+    override def postStop(): Unit = {
+      log.error("I'm stopped")
+    }
+
+    override def supervisorStrategy: SupervisorStrategy = OneForOneStrategy() {
+      case _: RuntimeException => Restart
+      case _ => Stop
     }
   }
   val system = ActorSystem("AkkaTrial")
   val actor = system.actorOf(Props[SimpleActor], "SimpleActor")
   actor ! "Hello world"
+  actor ! "stashThis"
+  actor ! "changeHandler"
+  actor ! PoisonPill
+  actor ! "Hi!"
 }
